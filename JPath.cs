4fab6896b18b8882db9151af4ddacb590199b221
lmatt-bit <lmatt123n@gmail.com>
2014-12-15 23:32:44 +0800

    implement the basic arch of JPath class

--- JPath.cs
+++ JPath.cs
@@ -4,6 +4,13 @@ using System.Linq;
 
 namespace org.lmatt
 {
+	/// <summary>
+	/// Now only support basic path( //, /, *, array index, object key) , no functions. 
+	/// Example:
+	/// 	Data: {"key1" : [{"key2": "value2"}]} 
+	/// 	JPath: //key1/0/key2
+	/// 	Return: JString("value2")
+	/// </summary>
 	public class JPath
 	{
 		private string[] jPaths;
@@ -23,7 +30,9 @@ namespace org.lmatt
 			IList<JBase> results = new List<JBase> ();
 			foreach (var jPath in jPaths) {
 				//TODO: unique the result
-				results.Add (Evaluate (jPath.ToCharArray (), 0, obj));
+				var ret = Evaluate (jPath.ToCharArray (), 0, obj);
+				if(ret != null)
+					results.Add (ret);
 			}
 
 			return results;
@@ -38,24 +47,100 @@ namespace org.lmatt
 			IList<JBase> results = new List<JBase> ();
 			foreach (var jPath in jPaths) {
 				//TODO: unique the result
-				results.Add (Evaluate (jPath.ToCharArray (), 0, array));
+				var ret = Evaluate (jPath.ToCharArray (), 0, array);
+				if(ret != null)
+					results.Add (ret);
 			}
 
 			return results;
 		}
 
 
-		private IList<JBase> Evaluate(char[] chars, int index, JBase baseObj)
+		private static IList<JBase> Evaluate(char[] chars, int index, JBase baseObj)
 		{
 			if (index >= chars.Length)
 				return null;
 
+			var ret = new List<JBase> ();
+
 			if (chars [index] == '/') {
 
 				int nextIndex = index + 1;
-				if (chars [nextIndex] == '/') {
+				if (nextIndex >= chars.Length)
+					return null;
 
+				if (chars [nextIndex] == '/') {
+					//TODO: implement double /
 				} else {
+					var sret = Evaluate (chars, index + 1, baseObj);
+					if(sret != null) {
+						ret.Add (sret);
+					}
+
+					return ret;
+				}
+			} else if (chars [index] == '*') {
+				var obj = baseObj as JObject;
+				if (obj != null) {
+
+					index++;
+					if (index == chars.Length) {
+						return obj.Values;
+					} else {
+						foreach (var v in obj.Values) {
+							var sret = Evaluate (chars, index, v);
+							if (sret != null) {
+								ret.Add (sret);
+							}
+						}
+
+						return ret;
+					}
+				}
+
+				var array = baseObj as JArray;
+				if (array != null) {
+
+					index++;
+					if (index == chars.Length) {
+						return array.ToList ();
+					} else {
+						foreach (var v in array) {
+							var sret = Evaluate (chars, index, v);
+							if (sret != null) {
+								ret.Add (sret);
+							}
+						}
+
+						return ret;
+					}
+				}
+
+				if (index + 1 != chars.Length)
+					return null;
+
+				ret.Add (baseObj);
+				return ret;
+			}
+			// This is array index
+			else if (chars [index] <= '9' && chars [index] >= '0') {
+				var array = baseObj as JArray;
+				if (array != null) {
+
+					//TODO: implement array index
+				} else {
+					return null;
+				}
+			}
+			// This is object key
+			else {
+
+				var obj = baseObj as JObject;
+				if (obj != null) {
+
+					//TODO: implement object key
+				} else {
+					return null;
 				}
 			}
 		}
